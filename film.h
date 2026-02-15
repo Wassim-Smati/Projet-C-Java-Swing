1005d75cd489cc34e50f406b10d2d68de61631b1
Wassim Smati <wassim.smati@telecom-paris.fr>
2026-02-16 06:19:40 +0900

    Fait de make run le point d'entrée serveur et clarifie l'usage

--- film.h
+++ film.h
@@ -1,35 +1,39 @@
 #ifndef FILM_H
 #define FILM_H
 #include "video.h"
-#include <vector>
 #include <iostream>
+#include <vector>
 
-class Film : public Video
-{
+class Film : public Video {
 protected:
-    Film(){};
-    Film(const std::string& name, const std::string& pathfile, const int length, const std::vector<int> chapitres)
-        : Video(name, pathfile, length), chapitres(chapitres){}
+    Film() = default;
+    Film(const std::string& name, const std::string& pathfile, int length, const std::vector<int>& chapitres)
+        : Video(name, pathfile, length), chapitres(chapitres) {}
     friend class MultimediaManager;
+
+    void writeData(std::ostream& out) const override;
+    void readData(std::istream& in) override;
+
 private:
     std::vector<int> chapitres{};
+
 public:
     virtual ~Film();
-    std::vector<int> getChapitres()const {return chapitres;}
-    void setChapitres(std::vector<int> chapitres){this->chapitres=chapitres;}
+    std::vector<int> getChapitres() const { return chapitres; }
+    void setChapitres(const std::vector<int>& newChapitres) { chapitres = newChapitres; }
 
-    void afficher(std::ostream& flux) const override{
+    void afficher(std::ostream& flux) const override {
         Video::afficher(flux);
         flux << "Durée des chapitres : ";
-        for(size_t i=0; i<chapitres.size();i++){
-            flux << "Chapitre n°" << i;
-            flux << "Durée du chapitre : " << chapitres[i];
+        for (size_t i = 0; i < chapitres.size(); i++) {
+            flux << "[Chapitre " << i << " : " << chapitres[i] << "] ";
         }
+        flux << "\n";
     }
 
+    std::string className() const override { return "Film"; }
 };
 
 using FilmPtr = std::shared_ptr<Film>;
 
 #endif // FILM_H
-//
